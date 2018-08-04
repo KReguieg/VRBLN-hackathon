87aac007ebbda94422d1799616b6db6891e99b38
DerCodeHahn <derhahn91@web.de>
2018-08-04 13:16:18 +0200

    added Event System

---
+++ Assets/Scripts/EventSystem/GameEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//[CreateAssetMenu(fileName = "GameEvent", menuName = "GameEvent", order = 0)]
+public class GameEvent : ScriptableObject {
+	public UnityEvent<GameEvent> Listener;
+
+	public void FireEvent(){
+		Listener.Invoke(this);
+	}
+
+}
