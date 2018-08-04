985315bfe2bc80607faaca98251af4feca3d767e
Viola <i_like_mini_muffins@web.de>
2018-08-05 01:14:36 +0200

    [EDIT] Flashlight follows right controller.

---
+++ Assets/Scripts/Maze/FlashlightMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightMovement : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject light;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			Debug.LogWarning("Light pos: " + light.transform.position + " " + "Control pos: " + transform.position);
+			light.transform.position = transform.position;	
+	}
+}
