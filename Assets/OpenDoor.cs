e82020cf7878762e4edbe314c57bffa81d365243
yellow <>
2018-08-05 01:28:52 +0200

    [ADD] first features into main

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RiddleController>().riddleSolved += TriggerOpenDoor;
+	}
+
+    private void TriggerOpenDoor()
+    {
+        GetComponent<Animator>().SetTrigger("open_door");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
