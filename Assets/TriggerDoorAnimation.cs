646ac94735f7ec32bb7311f11715a45554e75d52
yellow <>
2018-08-05 09:36:03 +0200

    [FIX] second door animation working :gun:

---
+++ Assets/TriggerDoorAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorAnimation : MonoBehaviour {
+    public Animator animator;
+
+    // Use this for initialization
+    void Start () {
+		GetComponent<RiddleController>().riddleSolved += TriggerDoorAnim;
+	}
+
+    private void TriggerDoorAnim()
+    {
+		animator.SetTrigger("open_door");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
