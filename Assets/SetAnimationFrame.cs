8d79afe237a5630dcaa6e94d850196729f35a0d8
Christopher Hahn <s55755@beuth-hochschule.de>
2018-08-05 02:29:44 +0200

    added misssing script

---
+++ Assets/SetAnimationFrame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Controllables;
+using VRTK.Controllables.ArtificialBased;
+
+public class SetAnimationFrame : MonoBehaviour {
+	public Animator animator;
+	VRTK_ArtificialSlider slider;
+	// Use this for initialization
+	void Start () {
+		animator.speed = 0;
+		slider = GetComponentInChildren<VRTK_ArtificialSlider>();
+		slider.ValueChanged += ValueChanged;
+	}
+
+    private void ValueChanged(object sender, ControllableEventArgs e)
+    {
+        animator.Play("Default State", -1 ,e.normalizedValue);
+    }
+}
