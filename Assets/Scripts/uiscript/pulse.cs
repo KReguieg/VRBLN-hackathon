3eee9af9efc52c8f50ce9d41f2544b78c2800f35
Rene Grabe <s67321@beuth-hochschule.de>
2018-08-05 02:58:35 +0200

    aflter mergeconflict (shit)

---
+++ Assets/Scripts/uiscript/pulse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pulse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale += new Vector3(Mathf.Sin(Time.time)*0.001f, Mathf.Sin(Time.time)*0.001f, Mathf.Sin(Time.time)*0.001f);
+	}
+}
