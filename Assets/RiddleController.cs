cff6225af987f43724fb63f813a7a14edc0d12f5
Viola <i_like_mini_muffins@web.de>
2018-08-05 06:38:55 +0200

    irdk

--- Assets/RiddleController.cs
+++ Assets/RiddleController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class RiddleController : MonoBehaviour {
 
-	VRTK_SnapDropZones[] dropZones;
+	//VRTK_SnapDropZones[] dropZones;
 
 	// Use this for initialization
 	void Start () {
+		/* 
 		dropZones = GetComponentsInChildren<VRTK_SnapDropZones>();
 		foreach (var dropZone in dropZones)
 		{
 
 		}
+		*/
 	}
 	
 	// Update is called once per frame
