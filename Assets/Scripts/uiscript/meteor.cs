b28c56a3640ef1f5d1cfcad499f856ee794bcb8f
Christopher Hahn <s55755@beuth-hochschule.de>
2018-08-05 10:17:15 +0200

    added Comect collision

--- Assets/Scripts/uiscript/meteor.cs
+++ Assets/Scripts/uiscript/meteor.cs
@@ -3,37 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class meteor : MonoBehaviour {
-	public GameObject target;
+	public Transform transfromTarget;
+	public Transform transfromStart;
+
+	public Vector3 target;
 	//public int speed;
 	//public int yspeed;
 	//public int zspeed;
-	private Vector3 startpos;
-	private int waiter = 0;
-	float step =0;
+	public float step = 0;
+	float rotationSpeed;
+	public bool RandomSpeed = true;
 	// Use this for initialization
 	void Start () {
-		startpos = transform.position;
-		step = Random.Range(25,125) * Time.deltaTime;
+		if(transfromStart){
+			transform.position = transfromStart.position;
+		}
+		if(transfromTarget)
+		{
+			target = transfromTarget.position;
+		}
+		if(RandomSpeed)
+			step = Random.Range (25, 125) * Time.deltaTime;
+		rotationSpeed = Random.Range (10, 50);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		// The step size is equal to speed times frame time.
 
 		// Move our position a step closer to the target.
-		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
-		transform.Rotate (new Vector3 (0, Time.deltaTime * Random.Range(1,100), Time.deltaTime * Random.Range(1,100)));
-		if(transform.position == target.transform.position){
-			
-			StartCoroutine (startmeteor());
+		transform.position = Vector3.MoveTowards (transform.position, target, step);
+		transform.Rotate (new Vector3 (0, 0, Time.deltaTime * rotationSpeed ));
+		if (Vector3.Distance(transform.position, target ) <= 0.1f) {
+			Destroy (gameObject);
 		}
 	}
-		IEnumerator startmeteor(){
-			yield return new WaitForSeconds (Random.Range(5,60));
-			transform.position = startpos;
-			
-		}
-		
-
-
 }
