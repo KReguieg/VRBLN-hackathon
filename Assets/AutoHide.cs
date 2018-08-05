fa52a5c39fd6adb6c11174e18fbc61c84fa1445a
Christopher Hahn <s55755@beuth-hochschule.de>
2018-08-05 11:00:44 +0200

    ...

---
+++ Assets/AutoHide.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoHide : MonoBehaviour {
+	public float hideTime = 10;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Hide());
+	}
+
+    IEnumerator Hide(){
+		yield return new WaitForSeconds(hideTime);
+	}
+
+}
