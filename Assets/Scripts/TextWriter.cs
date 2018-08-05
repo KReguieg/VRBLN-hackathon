665abe413d844113f6566055830b420f96af8106
yellow <>
2018-08-05 12:27:48 +0200

    [ADD] prolouge adding scene going through

--- Assets/Scripts/TextWriter.cs
+++ Assets/Scripts/TextWriter.cs
@@ -21,17 +21,19 @@ public class TextWriter : MonoBehaviour {
 		currentlyDisplayingText++;
 		//If we've reached the end of the array, do anything you want. I just restart the example text
 		if (currentlyDisplayingText>goatText.Length) {
-			currentlyDisplayingText=0;
+			UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+		} else {
+			StartCoroutine(AnimateText());
 		}
-		StartCoroutine(AnimateText());
 	}
 		//Note that the speed you want the typewriter effect to be going at is the yield waitforseconds (in my case it's 1 letter for every      0.03 seconds, replace this with a public float if you want to experiment with speed in from the editor)
 	IEnumerator AnimateText(){
-		
-		for (int i = 0; i < (goatText[currentlyDisplayingText].Length+1); i++)
-		{
-			textBox.text = goatText[currentlyDisplayingText].Substring(0, i);
-			yield return new WaitForSeconds(.03f);
+		if(currentlyDisplayingText < goatText.Length) {
+			for (int i = 0; i < (goatText[currentlyDisplayingText].Length); i++)
+			{
+				textBox.text = goatText[currentlyDisplayingText].Substring(0, i);
+				yield return new WaitForSeconds(.03f);
+			}
 		}
 
 		Invoke("SkipToNextText", 2);
