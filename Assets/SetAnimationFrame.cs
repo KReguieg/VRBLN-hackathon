c96b30680faf2818b2557757a79feb97564d228c
yellow <>
2018-08-05 02:46:18 +0200

    fixed bug

--- Assets/SetAnimationFrame.cs
+++ Assets/SetAnimationFrame.cs
@@ -8,6 +8,7 @@ using VRTK.Controllables.ArtificialBased;
 public class SetAnimationFrame : MonoBehaviour {
 	public Animator animator;
 	VRTK_ArtificialSlider slider;
+	public float openess;
 	// Use this for initialization
 	void Start () {
 		animator.speed = 0;
@@ -17,6 +18,7 @@ public class SetAnimationFrame : MonoBehaviour {
 
     private void ValueChanged(object sender, ControllableEventArgs e)
     {
-        animator.Play("Default State", -1 ,e.normalizedValue);
+        animator.Play("Default Take", -1 ,e.normalizedValue);
     }
+
 }
